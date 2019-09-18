e98abb78d4a5438b708c44612cc2e29a542ca48b
Sam Wallach Hanson <sam.wallach.hanson@chicagobooth.edu>
2019-09-18 19:50:05 +0000

    rotational cypher passes tests

--- rotational-cipher/rotational_cipher.py
+++ rotational-cipher/rotational_cipher.py
@@ -1,11 +1,19 @@
 from string import *
 def rotate(text, key):
     cyphertext = ""
-    text = str.lower(text)
     for letter in text:
-        letterint = ord(letter) - 96
+        letterint = ord(letter) - 96 
         if letterint > 0 and letterint < 27:
-            cyphertext = cyphertext + chr(((letterint + key)%26)+96)
+            if letterint + key != 26:
+                cyphertext = cyphertext + chr(((letterint + key)%26)+96)
+            else:
+                cyphertext = cyphertext + chr(((letterint + key))+96)
+        elif letterint > -32 and letterint < -5:
+            letterint = letterint + 33
+            if letterint + key != 26:
+                cyphertext = cyphertext + chr(((letterint + key)%26)+63)
+            else:
+                cyphertext = cyphertext + chr(((letterint + key))+63)
         else:
             cyphertext = cyphertext + letter
-    print(cyphertext)
+    return(cyphertext)
