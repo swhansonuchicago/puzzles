d6375aa928a1101be7e24a5b88c537393fb7cf18
Sam Wallach Hanson <sam.wallach.hanson@chicagobooth.edu>
2019-09-18 20:08:55 +0000

    isbn

--- isbn-verifier/isbn_verifier.py
+++ isbn-verifier/isbn_verifier.py
@@ -1,2 +1,23 @@
 def is_valid(isbn):
-    pass
+    countsofar = 0
+    total = 0
+    for letter in isbn:
+        if countsofar > 9:
+            print("Characters after 10th digit")
+            return
+        try:
+            letterint = int(letter)
+            total += (10-countsofar)*letterint
+            countsofar += 1
+        except ValueError:
+            if letter != "X" and letter != "-":
+                print("Invalid Character: " + letter)
+                return
+            if letter == "X" and countsofar < 9:
+                print("X before 10th digit")
+                return
+            elif letter == "X" and countsofar == 9:
+                total += 10
+                countsofar += 1
+    total = total%11
+    return(total)
