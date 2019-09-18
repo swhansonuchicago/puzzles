099f79900cee3cb4a975dafbc5419f8144e8d391
Sam Wallach Hanson <sam.wallach.hanson@chicagobooth.edu>
2019-09-18 16:14:07 +0000

    pangram

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -1,2 +1,19 @@
+from string import *
 def is_pangram(sentence):
-    pass
+    letters = []
+    sentence = str.lower(sentence)
+    letterlist = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u"]
+    letterlist.append("v")
+    letterlist.append("w")
+    letterlist.append("x")
+    letterlist.append("y")
+    letterlist.append("z")
+    for letter in sentence:
+        if (letter not in letters) and (letter in letterlist):
+            letters.append(letter)
+    print(letters)
+    if len(letters) == 26:
+        print("This is a pangram!")
+    else:
+        print("This is not a pangram :(")
+
