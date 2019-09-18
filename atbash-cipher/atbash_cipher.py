760c7d30f09fb40d8d73af17ea2c88d309cc9695
Ginha Kim <ginha.kim@chicagobooth.edu>
2019-09-18 19:17:47 +0000

    atbash-cipher intermediate

--- atbash-cipher/atbash_cipher.py
+++ atbash-cipher/atbash_cipher.py
@@ -1,6 +1,21 @@
+atbash = {'a':'z', 'b':'y', 'c':'x', 'd':'w', 'e':'v', 'f':'u', 'g':'t', 'h':'s', 'i':'r','j':'q','k':'p',
+          'l':'o','m':'n','n':'m','o':'l','p':'k','q':'j','r':'i','s':'h','t':'g','u':'f','v':'e','w':'d','x':'c','y':'b','z':'a'}
+
+
 def encode(plain_text):
     
-
+    cipher = ''
+    
+    plain_text = plain_text.lower()
+    for i in plain_text:
+        
+        if i != '':
+            cipher += atbash[i]
+        
+        else:
+            cipher += ''
+    
+    return cipher
 
 def decode(ciphered_text):
     
