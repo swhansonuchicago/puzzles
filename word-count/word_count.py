e9cccd7132ca28a5631b6e0ccd8e71a24f5455e6
Sam Wallach Hanson <sam.wallach.hanson@chicagobooth.edu>
2019-09-18 18:39:42 +0000

    word count

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -1,2 +1,30 @@
+from string import *
 def count_words(sentence):
-    pass
+    sentence = str.lower(sentence)
+    current = ""
+    dlist = {}
+    wlist = []
+    notletters = [" ", ",", ".", ":", ";"]
+    lastletter = " "
+    adder = 0
+    for letter in sentence:
+        if letter in notletters and lastletter not in notletters:
+            adder = 0
+            if current in wlist:
+                dlist[current] += 1
+            else:
+                wlist.append(current)
+                dlist[current] = 1
+            current = ""
+        elif letter not in notletters:
+            adder += 1
+            current = current + letter
+        lastletter = letter
+    if adder != 0:
+        if current in wlist:
+            dlist[current] += 1
+        else:
+            wlist.append(current)
+            dlist[current] = 1
+    for word in wlist:
+        print(word + ": " + str(dlist[word]))
